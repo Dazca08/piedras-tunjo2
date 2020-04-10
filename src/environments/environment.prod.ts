6134debe8471fdce70e0c005af7cb07f18e7c919
daniel zambrano <cdaniel1899@hotmail.com>
2020-04-09 21:30:48 -0500

    correcion mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   mapboxToken: 'pk.eyJ1IjoiYmxhY2tqYWNrZXJzMTciLCJhIjoiY2s1dHc1ZzNnMDNhcDNsbjUxYzI3eXo1YyJ9.34CZ5ZvoyTBgpgw13CkFHw',
+  mapboxTokenInicio:'pk.eyJ1IjoiZGF6Y2EwNyIsImEiOiJjazh0aXhudGQwMGsxM2V0NGswdjQ5czBpIn0._Flbt9rgR-mMBSqr4G8Haw',
   apiUrl: 'http://piedrasdeltunjo.tk'
 };
