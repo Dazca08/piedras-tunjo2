50477cebc7a67b8830047e940bf0616872db0b7a
Steven Cruz <blackjackers17@gmail.com>
2020-05-02 00:13:48 -0500

    app.component push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 const appId = 'c9b72292-e3e6-478d-a6cd-601e195ff462';
 declare var window: any;
+const OneSignal = window.OneSignal || [];
 
 @Component({
   selector: 'app-root',
@@ -9,20 +11,47 @@ declare var window: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
   title = 'AngularWeb';
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
   ngOnInit() {
-    const OneSignal = window.OneSignal || [];
-    OneSignal.push(() => {
+    this.oneSignalManage();
+  }
+
+  oneSignalManage() {
+    OneSignal.push( async () => {
       // If we're on an unsupported browser, do nothing
       if (!OneSignal.isPushNotificationsSupported()) {
-          return;
+        return;
       }
+      const auth = await this.authService.validateToken();
+      if (!auth) {
+        return;
+      }
+      const state =  await this.getSubscriptionState();
       OneSignal.init({
         appId,
         notifyButton: {
-          enable: true,
+          enable: (!state.isPushEnabled || state.isOptedOut)
         },
       });
     });
+  }
+
+  async getSubscriptionState() {
+    const result = await Promise.all([
+      OneSignal.isPushNotificationsEnabled(),
+      OneSignal.isOptedOut()
+    ]);
+    const isPushEnabled = result[0];
+    const isOptedOut = result[1];
+    return {
+      isPushEnabled,
+      isOptedOut
+    };
   }
 }
