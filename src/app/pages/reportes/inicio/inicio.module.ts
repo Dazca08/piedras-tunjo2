e4aa24ecd0c7d48692422d2737522f1e5f83fb3d
Steven Cruz <blackjackers17@gmail.com>
2020-05-06 08:34:06 -0500

    ddd

--- src/app/pages/reportes/inicio/inicio.module.ts
+++ src/app/pages/reportes/inicio/inicio.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio.component';
-import { AngularMaterialModule } from '../../../angular-material.module';
 
 @NgModule({
   declarations: [],
--- src/app/pages/reportes/reportes.module.ts
+++ src/app/pages/reportes/reportes.module.ts
@@ -4,12 +4,14 @@ import { ReportesRoutingModule } from './reportes-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
 // charts
 import { ChartsModule } from 'ng2-charts';
+import { AngularMaterialModule } from '../../angular-material.module';
 
 @NgModule({
   declarations: [InicioComponent],
   imports: [
     CommonModule,
     ChartsModule,
+    AngularMaterialModule,
     ReportesRoutingModule
   ]
 })
