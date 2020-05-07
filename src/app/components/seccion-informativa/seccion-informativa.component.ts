2f937f26c5a6dc75ccfd1a1a40d468bc7004c642
daniel zambrano <cdaniel1899@hotmail.com>
2020-05-06 23:51:29 -0500

    x

--- src/app/components/seccion-informativa/seccion-informativa.component.ts
+++ src/app/components/seccion-informativa/seccion-informativa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit , ComponentFactoryResolver} from '@angular/core';
 import { ServicioInfoService } from './servicio-info.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seccion-informativa',
@@ -18,26 +19,42 @@ export class SeccionInformativaComponent implements OnInit {
   rutaImagen:string="http://piedrasdeltunjo.tk/images/getImage?tipo=noticias&nombre=";
   fechatemp:any;
   i:number=0;
+  imagenes:any;
+  constructor(
+    private infoService: ServicioInfoService
+  ) {}
+
 ObtenerUltimasNoticias(){
+
   this.infoService.getNoticias().subscribe(resultado =>{
+    console.log("este es el resultado")
+    console.log(resultado)
  this.Noticias=resultado;
+
+ //console.log("es undefinida")
+ //this.Noticias="";
+ //console.log(this.Noticias.length)
+
+ console.log(this.Noticias)
   for(this.i=0;this.i<this.Noticias.length;this.i++){
      this.fechatemp=this.Noticias[this.i].fechaPublicacion;
   
      var splitted = this.fechatemp.split("T", 2); 
-  
+ 
     // console.log(splitted)
       this.fechatemp=splitted[0];
  
       this.Noticias[this.i].fechaPublicacion=this.fechatemp;
-     
+      this.imagenes=this.Noticias[this.i].imagenesUrl.split("@")
+     console.log(this.imagenes)
+
    }
 //  console.log(this.Noticias)
 //    console.log("Informacion ya tiene resultado");
     this.noticia1=this.Noticias[0];
-    this.noticia2=this.Noticias[1];
-    this.noticia3=this.Noticias[2];
-    // console.log(this.noticia1);
+    //this.noticia2=this.Noticias[1];
+   //this.noticia3=this.Noticias[2];
+     console.log(this.noticia1);
     // console.log(this.noticia2);
     // console.log(this.noticia3);
  
@@ -47,10 +64,7 @@ console.log(JSON.stringify(error));
 
  }); 
 }
-  constructor(
-    private infoService: ServicioInfoService
-  ) {}
-
+  
   ngOnInit() {
     this.infoService.getInformacion()
                     .subscribe((res: any[]) => {
