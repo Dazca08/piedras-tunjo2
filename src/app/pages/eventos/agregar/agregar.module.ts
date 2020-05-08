aca4e84747f7d75b0f7e06b74dfd1c5c041b97ac
daniel zambrano <cdaniel1899@hotmail.com>
2020-05-08 00:16:37 -0500

    arreglos

--- src/app/pages/eventos/agregar/agregar.module.ts
+++ src/app/pages/eventos/agregar/agregar.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AgregarComponent } from './agregar.component';
 import { PipesModule } from '../../../pipes/pipes.module';
+import { ComponentsModule } from '../../../components/components.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     PipesModule,
+    ComponentsModule,
     RouterModule.forChild([{ path: '', component: AgregarComponent }])
   ]
 })
